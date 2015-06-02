fa140fcdca13ef945622da028076273227255c11
jackie <jacktan.whu@foxmail.com>
2015-06-02 23:35:14 +0800

    Change the serialVersionUID of AccountPersistException
    this serialVersionUID is auto-generated by Eclipse

--- account-persist/src/main/java/io/jack/mvn/studycase/account/persist/AccountPersistException.java
+++ account-persist/src/main/java/io/jack/mvn/studycase/account/persist/AccountPersistException.java
@@ -4,7 +4,7 @@ public class AccountPersistException extends Exception {
     /**
      * 
      */
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 886350737426574940L;
 
     public AccountPersistException (String msg, Throwable ex) {
         super(msg, ex);
