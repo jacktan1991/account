f0fe70bcb10d9cba98934a90309abbe9484ea936
jackie <jacktan.whu@foxmail.com>
2015-06-03 10:57:36 +0800

    Complete AccountTest.testToString and test it green

--- account-core/src/main/java/io/jack/mvn/studycase/account/vo/Account.java
+++ account-core/src/main/java/io/jack/mvn/studycase/account/vo/Account.java
@@ -3,8 +3,6 @@ package io.jack.mvn.studycase.account.vo;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-import 
-
 public class Account {
     private String id;
     private String name;
@@ -18,72 +16,87 @@ public class Account {
     public String getId() {
         return id;
     }
+
     /**
-     * @param id the id to set
+     * @param id
+     *            the id to set
      * @return the account itself
      */
     public Account setId(String id) {
         this.id = id;
         return this;
     }
+
     /**
      * @return the name
      */
     public String getName() {
         return name;
     }
+
     /**
-     * @param name the name to set
+     * @param name
+     *            the name to set
      * @return the account itself
      */
     public Account setName(String name) {
         this.name = name;
         return this;
     }
+
     /**
      * @return the email
      */
     public String getEmail() {
         return email;
     }
+
     /**
-     * @param email the email to set
+     * @param email
+     *            the email to set
      * @return the account itself
      */
     public Account setEmail(String email) {
         this.email = email;
         return this;
     }
+
     /**
      * @return the password
      */
     public String getPassword() {
         return password;
     }
+
     /**
-     * @param password the password to set
+     * @param password
+     *            the password to set
      * @return the account itself
      */
     public Account setPassword(String password) {
         this.password = password;
         return this;
     }
+
     /**
      * @return the activated
      */
     public boolean isActivated() {
         return activated;
     }
+
     /**
-     * @param activated the activated to set
+     * @param activated
+     *            the activated to set
      * @return the account itself
      */
     public Account setActivated(boolean activated) {
         this.activated = activated;
         return this;
     }
-    
+
     public String toString() {
-        return ReflectionToStringBuilder.toString(this, ToStringStyle.JSON_STYLE);
+        return ReflectionToStringBuilder.toString(this,
+                ToStringStyle.JSON_STYLE);
     }
 }
--- account-core/src/test/java/io/jack/mvn/studycase/account/vo/AccountTest.java
+++ account-core/src/test/java/io/jack/mvn/studycase/account/vo/AccountTest.java
@@ -3,8 +3,9 @@
  */
 package io.jack.mvn.studycase.account.vo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -14,6 +15,13 @@ import org.junit.Test;
  */
 public class AccountTest {
 
+    private static final String CONST_ID = "#007";
+    private static final String CONST_NAME = "Jack";
+    private static final String CONST_EMAIL = "jack@gmail.com";
+    private static final String CONST_PWD = "admin";
+    private static final boolean CONST_ACTIVE = false;
+    Account testAccount;
+
     /**
      * @throws java.lang.Exception
      */
@@ -21,12 +29,29 @@ public class AccountTest {
     public static void setUpBeforeClass() throws Exception {
     }
 
+    @Before
+    public void setUp() {
+        testAccount = new Account();
+    }
+
     /**
-     * Test method for {@link io.jack.mvn.studycase.account.vo.Account#toString()}.
+     * Test method for
+     * {@link io.jack.mvn.studycase.account.vo.Account#toString()}.
      */
     @Test
     public final void testToString() {
-        fail("Not yet implemented"); // TODO
+
+        testAccount.setId(CONST_ID)
+                .setName(CONST_NAME)
+                .setEmail(CONST_EMAIL)
+                .setPassword(CONST_PWD)
+                .setActivated(CONST_ACTIVE);
+
+        String json_account = String
+                .format("{\"id\":\"%s\",\"name\":\"%s\",\"email\":\"%s\",\"password\":\"%s\",\"activated\":%b}",
+                        CONST_ID, CONST_NAME, CONST_EMAIL, CONST_PWD, CONST_ACTIVE);
+
+        assertEquals(json_account, testAccount.toString());
     }
 
 }
